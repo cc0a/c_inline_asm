ac9c5bcbd21b5e4f52a1d40b832d9c7c3645cd5a
cc0a <jmckeeco84@gmail.com>
2021-02-24 20:56:27 -0700

    updates

--- first_exercise.cpp
+++ first_exercise.cpp
@@ -5,4 +5,16 @@ int val = 0;
 
 int main(void) {
     // step 0: save old value oof rax
+    asm volatile("pushq %rax\n");
+    // step 1: move 5 to an architectual register
+    asm volatile("movq $5, %rax\n");
+
+    // step 2: move the value from the register to the variable (val)
+    asm volatile("movq %rax, val\n");
+
+    // step 3: restore the old value of rax
+    asm volatile("popq %rax\n");
+
+    cout << "The value is " << val << endl;
+    return 0;
 }
